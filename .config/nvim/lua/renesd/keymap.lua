97c347b8b4b8dad8941807953fdfdecf1c4d64ff
renesd <rd.renesdaniel@gmail.com>
2024-04-24 17:30:38 +0200

    neovim: add diagnostics keymaps

--- .config/nvim/lua/renesd/keymap.lua
+++ .config/nvim/lua/renesd/keymap.lua
@@ -2,6 +2,11 @@ vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 
+vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>d[', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>d]', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>dd', function() vim.cmd('Telescope diagnostics') end)
+
 vim.keymap.set('n', '<C-t>', function() vim.cmd('tabnew') end)
 vim.keymap.set('n', '<C-Left>', function() vim.cmd('tabprevious') end)
 vim.keymap.set('n', '<C-Right>', function() vim.cmd('tabnext') end)
